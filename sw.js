55707c46c3c98c718a238202ad079f678141aba9
naoki <n.globe.us@gmail.com>
2025-09-01 10:48:53 +0900

    some fix

--- sw.js
+++ sw.js
@@ -12,5 +12,10 @@ self.addEventListener('fetch', (event) => {
 
   event.respondWith(
     fetch(event.request).catch(() => caches.match('/offline.html'))
+
+    // NOTE: 以下のようにして任意のテキストを返すことも可能
+    // new Response('Hello from Service Worker', {
+    //   headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+    // })
   );
 });
